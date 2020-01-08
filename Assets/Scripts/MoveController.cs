27fce99e01f1978fbe26ef921b8fd37b3f3a055a
Sky9th <weitianxu@qq.com>
2020-01-08 21:55:53 +0800

    [+] 角色控制

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("鼠标左键");
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            print("鼠标右键");
+            Vector3 mousePosition = Input.mousePosition;
+            print(mousePosition);
+            Ray ray = new Ray(mousePosition);
+        }
+    }
+}
