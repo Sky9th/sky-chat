bcf33412a926e97bdb2caf04c8f961978a447148
Sky9th <weitianxu@qq.com>
2022-12-08 23:37:46 +0800

    [+] web post

--- Assets/Scripts/StartUIController.cs
+++ Assets/Scripts/StartUIController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
@@ -17,14 +18,49 @@ public class StartUIController : MonoBehaviour
         button.RegisterCallback<ClickEvent>(BtnStart);
     }
 
+    IEnumerator Login()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+        UnityWebRequest www = UnityWebRequest.Post("http://api.skyadmin.sky9th.cn/user/login", formData);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+            Res res = JsonUtility.FromJson<Res>(www.downloadHandler.text);
+            Debug.Log(res.code);
+        }
+    }
+
     private void BtnStart(ClickEvent evt)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(Login());
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+
+[System.Serializable]
+public class Res
+{
+
+    public string code;
+    public string msg;
+    public string data;
+
+    public static Res CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<Res>(jsonString);
     }
 }
